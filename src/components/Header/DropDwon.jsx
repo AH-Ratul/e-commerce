ea96defda96ecfb4206772aa2c289387383fdc96
AH-Ratul <ahratul21@gmail.com>
2023-09-15 16:49:03 +0600

    new items added

---
+++ src/components/Header/DropDwon.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const DropDwon = () => {
+  return (
+    <div className="right-0 ml-36 bg-white mt-3 p-5 absolute shadow-md ">
+      <p className="text-sm mb-3 font-bold text-gray-400 text-center">
+        Welcome to EARN-SHOP
+      </p>
+      <div className="flex justify-between items-center p-1 border-b pb-4">
+        <Link
+          to="/signup"
+          className="bg-red-500 px-3 py-1 text-white font-semibold"
+        >
+          Register
+        </Link>
+        <Link
+          to="/login"
+          className="bg-orange-200 px-3 py-1 ml-3 font-semibold text-red-600 "
+        >
+          Login
+        </Link>
+      </div>
+      <div className="flex flex-col mt-1">
+        <Link className="text-sm hover:bg-gray-100 p-1">My Orders</Link>
+        <Link className="text-sm hover:bg-gray-100 p-1">Payment</Link>
+        <Link className="text-sm hover:bg-gray-100 p-1">Profile</Link>
+      </div>
+    </div>
+  );
+};
+
+export default DropDwon;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import {
   faCartPlus,
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import DropDwon from "./DropDwon";
 
 const Header = () => {
+  const [dropDownOpen, setDropDownOpen] = useState(false);
+
+  const isToggleMenu = () => {
+    setDropDownOpen(!dropDownOpen);
+  };
+
   return (
     <div className="flex justify-between items-center pl-16 pr-16 p-5 fixed z-10 top-0 right-0 left-0 bg-white shadow-md">
       <h1 className="font-bold text-3xl text-lime-500">EARN-SHOP</h1>
@@ -37,7 +44,7 @@ const Header = () => {
           Contacts
         </NavLink>
       </div>
-      <div className="relative">
+      <div className="relative flex items-center">
         <input
           type="text"
           className="py-2 px-5 rounded-xl mr-8 border outline-none"
@@ -49,34 +56,21 @@ const Header = () => {
         <NavLink className="text-2xl mr-5 text-gray-700">
           <FontAwesomeIcon icon={faCartPlus} />
         </NavLink>
-        <span>
-          <Link className="text-2xl hover:text-red-500">
+        
+        <div className=" relative">
+          <button
+            onClick={isToggleMenu}
+            className="text-2xl hover:text-red-500"
+          >
             <FontAwesomeIcon icon={faUser} />
-          </Link>
-          <div className="right-0 ml-36 bg-white mt-3 p-5 absolute shadow-md ">
-            <p className="text-sm mb-3 font-bold text-gray-400 text-center">
-              Welcome to EARN-SHOP
-            </p>
-            <div className="flex justify-between items-center p-1 border-b pb-4">
-              <Link
-                to="/signup"
-                className="bg-red-500 px-3 py-1 text-white font-semibold"
-              >
-                Register
-              </Link>
-              <Link
-                to="/login"
-                className="bg-orange-200 px-3 py-1 ml-3 font-semibold text-red-600 "
-              >
-                Login
-              </Link>
-            </div>
-            <div className="flex flex-col mt-1">
-              <Link className="text-sm hover:bg-gray-100 p-1">My Orders</Link>
-              <Link className="text-sm hover:bg-gray-100 p-1">Payment</Link>
-            </div>
-          </div>
-        </span>
+          </button>
+
+          {/* toggle drop down menu */}
+          {dropDownOpen && (
+            <DropDwon></DropDwon>
+          )}
+        </div>
+
       </div>
     </div>
   );
